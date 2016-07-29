66b79134313018bf826ed56eafd4876c0064158d
Himmelt <master@void-3.cn>
2016-07-29 14:14:23 +0800

    renderInventoryBlock

--- src/main/java/org/soraworld/chinachess/client/render/ModRender.java
+++ src/main/java/org/soraworld/chinachess/client/render/ModRender.java
@@ -11,7 +11,9 @@ import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.block.Block;
 import net.minecraft.client.renderer.RenderBlocks;
+import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.world.IBlockAccess;
+import org.lwjgl.opengl.GL11;
 import org.soraworld.chinachess.registry.ModRegistry;
 
 @SideOnly(Side.CLIENT)
@@ -19,17 +21,19 @@ public class ModRender implements ISimpleBlockRenderingHandler {
 
     @Override
     public void renderInventoryBlock(Block block, int metadata, int modelId, RenderBlocks renderer) {
-        renderer.setRenderBoundsFromBlock(block);
-        renderer.renderBlockAsItem(block,0,1.0F);
+        Tessellator tessellator = Tessellator.instance;
+        renderer.setRenderBounds(0.0F, 0.0F, 0.0F, 1.0F, 1.0F, 1.0F);
+        this.renderInInventory(tessellator, renderer, block, metadata);
     }
 
     @Override
     public boolean renderWorldBlock(IBlockAccess world, int x, int y, int z, Block block, int modelId, RenderBlocks renderer) {
         renderer.setRenderBoundsFromBlock(block);
-        int meta = world.getBlockMetadata(x, y, z);
-        if(meta == 1) renderer.uvRotateTop = 1;
-        if(meta == 3) renderer.uvRotateTop = 2;
-        if(meta == 2) renderer.uvRotateTop = 3;
+        switch (world.getBlockMetadata(x,y,z)){
+            case 1: renderer.uvRotateTop = 1;break;
+            case 2: renderer.uvRotateTop = 3;break;
+            case 3: renderer.uvRotateTop = 2;break;
+        }
         renderer.renderStandardBlock(block, x, y, z);
         // Must reset the rotation or it will mess up all rotating blocks around
         renderer.uvRotateTop = 0;
@@ -46,4 +50,33 @@ public class ModRender implements ISimpleBlockRenderingHandler {
         return ModRegistry.modRenderId;
     }
 
+    private void renderInInventory(Tessellator tessellator, RenderBlocks renderer, Block block, int metadata)
+    {
+        GL11.glTranslatef(-0.5F, -0.5F, -0.5F);
+        tessellator.startDrawingQuads();
+        tessellator.setNormal(0.0F, -1F, 0.0F);
+        renderer.renderFaceYNeg(block, 0.0D, 0.0D, 0.0D, block.getIcon(0, metadata));
+        tessellator.draw();
+        tessellator.startDrawingQuads();
+        tessellator.setNormal(0.0F, 1.0F, 0.0F);
+        renderer.renderFaceYPos(block, 0.0D, 0.0D, 0.0D, block.getIcon(1, metadata));
+        tessellator.draw();
+        tessellator.startDrawingQuads();
+        tessellator.setNormal(0.0F, 0.0F, -1F);
+        renderer.renderFaceZNeg(block, 0.0D, 0.0D, 0.0D, block.getIcon(2, metadata));
+        tessellator.draw();
+        tessellator.startDrawingQuads();
+        tessellator.setNormal(0.0F, 0.0F, 1.0F);
+        renderer.renderFaceZPos(block, 0.0D, 0.0D, 0.0D, block.getIcon(3, metadata));
+        tessellator.draw();
+        tessellator.startDrawingQuads();
+        tessellator.setNormal(-1F, 0.0F, 0.0F);
+        renderer.renderFaceXNeg(block, 0.0D, 0.0D, 0.0D, block.getIcon(4, metadata));
+        tessellator.draw();
+        tessellator.startDrawingQuads();
+        tessellator.setNormal(1.0F, 0.0F, 0.0F);
+        renderer.renderFaceXPos(block, 0.0D, 0.0D, 0.0D, block.getIcon(5, metadata));
+        tessellator.draw();
+        GL11.glTranslatef(0.5F, 0.5F, 0.5F);
+    }
 }
